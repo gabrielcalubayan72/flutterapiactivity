3f2c13e4eedb61decd95bdbf5cbdbd8158cea1d8
gabrielcalubayan <gabriel.calubayan@maya.ph>
2026-02-06 11:44:34 +0800

    chore: cleaned files

--- lib/domain/repositories/auth_repository.dart
+++
@@ -1,9 +0,0 @@
-
-import 'package:cadetbank/core/utils/result/result.dart';
-
-abstract interface class AuthRepository {
-  Future<Result<void>> login({
-    required String username,
-    required String password,
-  });
-}
